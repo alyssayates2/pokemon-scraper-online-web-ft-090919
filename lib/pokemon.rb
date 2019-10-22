a776632eb9aa695a4afac337be50c596b94ab96d
Alyssa Yates <bosslys@icloud.com>
2019-10-22 18:25:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,3 +1,9 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db 
+  attr_accessor :id, :name, :type, :db
+
+  def self.initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
 end
