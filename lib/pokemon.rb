52658449a2cac67291cc26847d46294a8385403f
Alyssa Yates <bosslys@icloud.com>
2019-10-22 18:50:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,33 +8,8 @@ class Pokemon
     @db = db
   end
 
-  def self.create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS pokemons(
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        type TEXT
-        )
-        SQL
-      DB[:conn].execute(sql)
-  end
-
-  def self.drop_table
-    sql = <<-SQL
-      DROP TABLE IF EXISTS pokemons
-    SQL
-    DB[:conn].execute(sql)
-  end
-
   def save
-    sql = <<-SQL
-    INSERT INTO pokemons (name, type)
-    VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.type)
-
-    @id = DB[:conn].execute("SELECT id FROM pokemons")[0][0]
+    
   end
 
 end
