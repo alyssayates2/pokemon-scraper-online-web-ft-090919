8e3f4e44d1e3c3e54f43527fe1e33126f1387279
Alyssa Yates <bosslys@icloud.com>
2019-10-22 19:50:21 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,23 +8,6 @@ class Pokemon
     @db = db
   end
 
-  def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS pokemons (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      type INTEGER
-    )
-    SQL
-
-    DB[:conn].execute(sql)
-  end
-
-  def self.drop_table
-    sql = "DROP TABLE IF EXISTS pokemons"
-      DB[:conn].execute(sql)
-  end
-
   def save
      if self.id
        self.update
@@ -37,17 +20,6 @@ class Pokemon
        DB[:conn].execute(sql, self.name, self.type, self.db)
        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemons")[0][0][0]
       end
-    end
-
-    def update
-      sql = "UPDATE pokemons SET name = ?, type = ? WHERE id = ?"
-      DB[:conn].execute(sql, self.name, self.type, self.id)
-    end
-
-    def self.create(name, type)
-      pokemon = Pokemon.new(name, type)
-      pokemon.save
-      pokemon
     end
 
 end
